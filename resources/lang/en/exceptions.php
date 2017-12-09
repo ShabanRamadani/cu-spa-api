100d9e6f23731853631dae5718dc9093a3cfa779
Shaban Ramadani <shaban.ramadani@comm-it.it>
2017-12-09 16:04:58 +0100

    added exception messages

---
+++ resources/lang/en/exceptions.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'no_query_result_for_model' => 'No :model found',
+    'route_not_found'           => 'Route not found',
+    'method_not_allowed'        => 'Http :method method is not allowed',
+];
