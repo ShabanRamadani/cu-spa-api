9a745d576b7fa2f9236a0ba8a10c939e9f41d29e
Shaban Ramadani <shaban.ramadani@comm-it.it>
2017-12-09 16:04:28 +0100

    updated exceptions handling

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,9 +2,18 @@
 
 namespace Spa\Exceptions;
 
+use EllipseSynergie\ApiResponse\Contracts\Response;
 use Exception;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Session\TokenMismatchException;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Watson\Validating\ValidationException as SelfValidationException;
 
 class Handler extends ExceptionHandler
 {
@@ -14,12 +23,12 @@ class Handler extends ExceptionHandler
      * @var array
      */
     protected $dontReport = [
-        \Illuminate\Auth\AuthenticationException::class,
-        \Illuminate\Auth\Access\AuthorizationException::class,
-        \Symfony\Component\HttpKernel\Exception\HttpException::class,
-        \Illuminate\Database\Eloquent\ModelNotFoundException::class,
-        \Illuminate\Session\TokenMismatchException::class,
-        \Illuminate\Validation\ValidationException::class,
+        AuthenticationException::class,
+        AuthorizationException::class,
+        HttpException::class,
+        ModelNotFoundException::class,
+        TokenMismatchException::class,
+        ValidationException::class,
     ];
 
     /**
@@ -27,7 +36,8 @@ class Handler extends ExceptionHandler
      *
      * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
      *
-     * @param  \Exception  $exception
+     * @param  \Exception $exception
+     *
      * @return void
      */
     public function report(Exception $exception)
@@ -38,20 +48,46 @@ class Handler extends ExceptionHandler
     /**
      * Render an exception into an HTTP response.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Exception               $exception
+     *
      * @return \Illuminate\Http\Response
      */
     public function render($request, Exception $exception)
     {
+        /** @var Response $response */
+        $response = app(Response::class);
+
+        if ($exception instanceof SelfValidationException) {
+            return $response->errorWrongArgs($exception->getMessageBag());
+        }
+        if ($exception instanceof AuthorizationException) {
+            return $response->errorForbidden($exception->getMessage());
+        }
+
+        if ($exception instanceof NotFoundHttpException) {
+            return $response->errorNotFound(trans('exceptions.route_not_found'));
+        }
+
+        if ($exception instanceof MethodNotAllowedHttpException) {
+            return $response->errorMethodNotAllowed(trans('exceptions.method_not_allowed', ['method' => $request->getMethod()]));
+        }
+
+        if ($exception instanceof ModelNotFoundException) {
+            $model = last(explode("\\", $exception->getModel()));
+
+            return $response->errorNotFound(trans('exceptions.no_query_result_for_model', ['model' => $model]));
+        }
+
         return parent::render($request, $exception);
     }
 
     /**
      * Convert an authentication exception into an unauthenticated response.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Illuminate\Auth\AuthenticationException  $exception
+     * @param  \Illuminate\Http\Request $request
+     * @param  AuthenticationException  $exception
+     *
      * @return \Illuminate\Http\Response
      */
     protected function unauthenticated($request, AuthenticationException $exception)
